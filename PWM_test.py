165bac49bf5820e966c2a038d0586b2e7320ca49
laughingblue <caeruleus.lab@outlook.com>
2017-03-27 23:50:48 +0800

    Fisrt Test Successed

--- PWM_test.py
+++ PWM_test.py
@@ -1,18 +1,3 @@
-#!/usr/bin/python
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#|R|a|s|p|b|e|r|r|y|P|i|.|c|o|m|.|t|w|
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-# Copyright (c) 2014, raspberrypi.com.tw
-# All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-#
-# pwm_led.py
-# Adjust the bright of a led by software PWM
-#
-# Author : sosorry
-# Date   : 06/22/2014
-
 import RPi.GPIO as GPIO
 import time
 
