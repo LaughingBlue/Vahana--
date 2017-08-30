c4e8e85702d5ec659bd40b2c1e4746c641e51708
LaughingBlue <caeruleus.lab@outlook.com>
2017-08-31 02:21:18 +0800

    rebuild

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,12 @@ var sip = require('underscore')
 .value()
 .address;
 console.log('Server IP='+sip);
-serverip_array = sip.split('.');
-trasData = {serverip0: serverip_array[0], serverip1: serverip_array[1], serverip2: serverip_array[2], serverip3: serverip_array[3]};
+//serverip_array = sip.split('.');
+//trasData = {serverip0: serverip_array[0], serverip1: serverip_array[1], serverip2: serverip_array[2], serverip3: serverip_array[3]};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', trasData);
+  res.render('index', sip);
 });
 
 module.exports = router;
