cb5c6bcfdbcd728a5d0663bdcd6e9c4893056be2
LaughingBlue <caeruleus.lab@outlook.com>
2017-08-31 01:37:59 +0800

    rebuild

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+trasData = { title: 'Express', ServerIP: "192.168.1.197" };
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', trasData);
 });
 
 module.exports = router;
